a7bdeef38ed3c93a1a890038be48cf8f2b723b07
404-Rookie <3ell.rookie@gmail.com>
2024-04-03 16:16:03 -0300

    css

--- jQuery/main.js
+++ jQuery/main.js
@@ -2,3 +2,13 @@ $(document).ready(function() {
     console.log($('#botao-cancelar'));
 })
 
+
+$(document).ready(function() {
+    $('header button').click(function() {
+        alert("Expandir formulário")
+    })
+
+    $('form').on('submit', funtion(e) {
+        console.log("submit");
+    })
+}
